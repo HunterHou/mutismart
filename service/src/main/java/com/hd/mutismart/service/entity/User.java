a5e8a91e722cf15fa28000ac4f64c01106995720
hunter <183598821@qq.com>
2019-07-22 19:58:40 +0800

    md

--- service/src/main/java/com/hd/mutismart/service/entity/User.java
+++ service/src/main/java/com/hd/mutismart/service/entity/User.java
@@ -1,9 +1,12 @@
 package com.hd.mutismart.service.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
 }
--- site/src/main/java/com/hd/mutismart/site/controller/UserController.java
+++ site/src/main/java/com/hd/mutismart/site/controller/UserController.java
@@ -2,6 +2,7 @@ package com.hd.mutismart.site.controller;
 
 import com.hd.mutismart.base.result.MessageCode;
 import com.hd.mutismart.base.result.ReqResult;
+import com.hd.mutismart.service.entity.User;
 import com.hd.mutismart.service.service.IUserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,14 @@ public class UserController {
     @Resource
     private IUserService userService;
 
-    @RequestMapping("user")
-    public ReqResult test() {
-        return ReqResult.success();
+    @RequestMapping("user/add")
+    public ReqResult add(User user) {
+        return userService.insert(user);
+    }
+
+    @RequestMapping("user/delete")
+    public ReqResult delete(User user) {
+        return userService.delete(user.getId());
     }
 
     @RequestMapping("user/all")
