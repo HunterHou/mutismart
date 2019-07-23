e8405069cd7365d164ba3e2a7a74d451bf82d003
hunter <183598821@qq.com>
2019-07-23 10:31:26 +0800

    md

--- code-generator/src/main/java/com/hd/mutismart/codegenerator/CodeGenerator.java
+++ code-generator/src/main/java/com/hd/mutismart/codegenerator/CodeGenerator.java
@@ -48,7 +48,7 @@ public class CodeGenerator {
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setOutputDir(projectPath + "codeSource/src/main/java");
         gc.setAuthor("hunter");
         gc.setOpen(false);
         // gc.setSwagger2(true); 实体属性 Swagger2 注解
