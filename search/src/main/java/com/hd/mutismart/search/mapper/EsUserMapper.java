6d3530a1ebfee7257245f667c28782defe1d7f4b
HunterHou <183598821@qq.com>
2019-07-24 01:10:47 +0800

    hd

--- search/src/main/java/com/hd/mutismart/search/mapper/EsUserMapper.java
+++ search/src/main/java/com/hd/mutismart/search/mapper/EsUserMapper.java
@@ -2,8 +2,6 @@ package com.hd.mutismart.search.mapper;
 
 import com.hd.mutismart.search.entity.EsUser;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EsUserMapper extends ElasticsearchRepository<EsUser, Long> {
 }
