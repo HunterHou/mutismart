a16fef074b5c946d9265d180f9f849a1167b0ece
hunter <183598821@qq.com>
2019-07-24 18:48:20 +0800

    md

--- base/src/main/java/com/hd/mutismart/base/result/ReqResult.java
+++ base/src/main/java/com/hd/mutismart/base/result/ReqResult.java
@@ -5,6 +5,8 @@ import java.util.List;
 import lombok.Data;
 
 /**
+ * 通用返回参数
+ * 
  * @author hunter
  */
 @Data
@@ -30,8 +32,16 @@ public class ReqResult {
         return new ReqResult(MessageCode.SUCCESS);
     }
 
+    public static ReqResult success(List<Long> ids) {
+        return ReqResult.success().setIds(ids);
+    }
+
     public static ReqResult success(Object data) {
-        return new ReqResult(MessageCode.SUCCESS).setData(data);
+        return ReqResult.success().setData(data);
+    }
+
+    public static ReqResult success(Long id, Object data) {
+        return ReqResult.success().setData(data).setId(id);
     }
 
     public static ReqResult fail() {
@@ -39,13 +49,26 @@ public class ReqResult {
     }
 
     public static ReqResult fail(String message) {
-        ReqResult reqResult = ReqResult.fail();
-        reqResult.setMessage(message);
-        return reqResult;
+        return ReqResult.fail().setMessage(message);
+    }
+
+    public ReqResult setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public ReqResult setIds(List<Long> ids) {
+        this.ids = ids;
+        return this;
     }
 
     public ReqResult setData(Object data) {
         this.data = data;
+        return this;
+    }
+
+    public ReqResult setMessage(String message) {
+        this.message = message;
         return this;
     }
 
