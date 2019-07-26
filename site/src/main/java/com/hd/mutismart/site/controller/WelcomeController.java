1966d331e1632de382aacccd8b3b86f47870c9e6
hunter <183598821@qq.com>
2019-07-26 15:12:25 +0800

    md

--- site/src/main/java/com/hd/mutismart/site/controller/WelcomeController.java
+++ site/src/main/java/com/hd/mutismart/site/controller/WelcomeController.java
@@ -1,13 +1,13 @@
 package com.hd.mutismart.site.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/welcome")
+    @GetMapping("welcome")
     public ModelAndView home() {
         return new ModelAndView("welcome.ftl");
     }
